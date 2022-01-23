4623c5687f87a22fd1a9951ca63180f49c4e895c
Gheorghe <info@semcogheorghe.com>
2022-01-23 14:29:08 +0100

    initial commit

---
+++ src/test/java/com/fabrick/restcontroller/RestcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrick.restcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
